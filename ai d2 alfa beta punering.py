0a205ceca0bba0c8a76d4502e47b8d7f74cc4b03
tharun7841 <112371398+tharun7841@users.noreply.github.com>
2023-01-11 21:44:17 +0530

    Add files via upload

---
+++ ai d2 alfa beta punering.py
@@ -0,0 +1,20 @@
+import time
+
+class Game:
+    def __init__(self):
+        self.initialize_game()
+
+    def initialize_game(self):
+        self.current_state = [['.','.','.'],
+                              ['.','.','.'],
+                              ['.','.','.']]
+
+        # Player X always plays first
+        self.player_turn = 'X'
+
+    def draw_board(self):
+        for i in range(0, 3):
+            for j in range(0, 3):
+                print('{}|'.format(self.current_state[i][j]), end=" ")
+            print()
+        print()
